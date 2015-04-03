66bb6537cb8994154fc4f471e7c3c9fd73e5d05e
egyptiankarim <karim@egyptiankarim.com>
2015-04-03 13:35:45 -0400

    Cleaned up some view partials.

--- app/controllers/controls_controller.rb
+++ app/controllers/controls_controller.rb
@@ -6,11 +6,17 @@ class ControlsController < ApplicationController
   def show
     @control = Control.where(number: params[:number]).first
 
-    # These are ugly hacks for MySQL and the way NIST numbers their statements.
+    # These are ugly hacks for MySQL and the way NIST numbers their statements. Lots of ugh.
     @statements = Statement.where("number REGEXP ?", "#{params[:number]}([a-z]|$)")
     @references = Reference.where(number: params[:number])
     @supplement = Supplement.where("number REGEXP ?", "#{params[:number]}$").first
     @related_controls = @supplement.related.split(",")
+
+    # More ugh, built out for enhancmenets, which are really just controls nested in controls.
     @enhancements = Control.where("is_enhancement = ? AND number REGEXP ?", true, "#{params[:number]} ")
+    @enhancement_supplements = {}
+    @enhancements.each do |enhancement|
+      @enhancement_supplements[enhancement.number] = Supplement.where(number: enhancement.number).first
+    end
   end
 end
