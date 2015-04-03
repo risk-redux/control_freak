6b74631abc79c2b4145332e1b34561485ffa18f8
egyptiankarim <karim@egyptiankarim.com>
2015-04-03 16:27:01 -0400

    Set up basic control enhancement display.

--- app/helpers/controls_helper.rb
+++ app/helpers/controls_helper.rb
@@ -2,7 +2,7 @@ module ControlsHelper
   def control_statement(statement)
     @number = statement.number
     @is_odv = statement.is_odv
-    @parsed_number = @number[/([A-Z]{2}-[0-9]{1,2})(([a-z]\.|[0-9]\.)*|$)/, 3] # Ugh.
+    @parsed_number = @number[/([A-Z]{2}-[0-9]{1,2})(([a-z]\.|[0-9]*\.)*|$)/, 3] # Ugh.
     @level = @number.scan(".").length # Double ugh.
     @description = statement.description
     @parsed_description = @is_odv ? parsed_description(@description) : @description
@@ -10,9 +10,20 @@ module ControlsHelper
     render("controls/shared/statement", number: @parsed_number, level: @level, description: @description)
   end
 
+  def enhancement_statement(statement)
+    @number = statement.number
+    @is_odv = statement.is_odv
+    @parsed_number = @number[/([A-Z]{2}-[0-9]{1,2}) (\([0-9]*\))((\([a-z]\)|\([0-9]*\))*|$)/, 4] # Ugh.
+    @level = @number.scan("(").length - 1 # Double ugh.
+    @description = statement.description
+    @parsed_description = @is_odv ? parsed_description(@description) : @description
+
+    render("controls/shared/statement", number: @parsed_number, level: @level, description: @description)
+  end
+
   def parsed_description(description)
-    @parsed_description = description.insert(description.index('['), "<mark>")
-    @parsed_description = @parsed_description.insert(description.index(']')+1, "</mark>")
+    @parsed_description = description.split("[").join("<mark>[")
+    @parsed_description = @parsed_description.split("]").join("]</mark>")
     @parsed_description
   end
 
