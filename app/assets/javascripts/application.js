ee98271cdb06b068ff66077f7aeb4f39f6b98d75
Karim Said <karim.a.said@nasa.gov>
2019-09-03 17:09:58 -0400

    Facelift.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap
+//= require rails-ujs
+//= require activestorage
 //= require_tree .
