6af0531b3d4e60ccde0f8da0ad0225893cb24bb3
Karim Said <karim@egyptiankarim.com>
2019-09-04 08:19:51 -0400

    Fixing precompile directives.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,11 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# To support controller specific css/js rendering
+Rails.application.config.assets.precompile += ['*.css']
+Rails.application.config.assets.precompile += ['*.js']
+
+# All my lib images
 Rails.application.config.assets.precompile += ['*.png']
+Rails.application.config.assets.precompile += ['*.ico']
